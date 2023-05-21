c5db0bdf553fd74f985f5fc7953314a90e8f4fd2
SP!RAL <mateuslimarod2602@gmail.com>
2023-05-21 01:27:24 -0300

    :)

---
+++ LISTA 1/Questão 1/RemoveMenor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> removeMenor(vector<int> L, int x)
+{
+    vector<int> lista2;
+    for (int valor : L)
+    {
+        if (valor > x)
+        {
+            lista2.push_back(valor);
+        }
+    }
+    return lista2;
+}
+
+int main()
+{
+    int n;
+    cout << "Digite o tamanho da sua lista : ";
+    cin >> n;
+    vector<int> lista(n);
+    cout << "Digite os elementos da sua lista : ";
+    for(int i = 0; i < n; i++){
+        cin >> lista[i];
+    }
+    cout << "Digite o numero que deseja : ";
+    int x;
+    cin >> x;
+    vector<int> lista2 = removeMenor(lista, x);
+    for (int valor : lista2)
+    {
+        cout << valor << " ";
+    }
+    cout << std::endl;
+}
